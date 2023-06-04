6642766964914392367a388525ce82c5035fa1ad
waitaamon <waitaamon@gmail.com>
2023-06-04 10:51:01 +0300

    added tenant model, migration, policy

---
+++ app/Models/Tenant.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Tenant extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'pin', 'phone', 'email', 'address'];
+
+    protected $appends = ['balance'];
+
+    protected function lastPaymentDate(): Attribute
+    {
+        return new Attribute(get: function () {
+            $payment = $this->payments()->latest()->first();
+            return $payment ? $payment->created_at : null;
+        });
+    }
+
+    protected function balance(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                $statement = $this->statements()->latest()->first();
+
+                return $statement ? $statement->balance : 0;
+            },
+        );
+    }
+
+    protected function totalInvoices(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->invoices()->where('status', 'approved')->get()->sum('total_amount'),
+        );
+    }
+
+    protected function totalPayments(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->payments()->where('status', 'approved')->sum('amount'),
+        );
+    }
+
+    public function balanceAsAt($date = null): float
+    {
+        $statement = $this->statements()
+            ->when(!is_null($date), fn(Builder $query) => $query->whereDate('created_at', '<=', $date instanceof Carbon ? $date : Carbon::parse($date)))
+            ->latest()
+            ->first();
+
+        return $statement ? $statement->balance : 0;
+    }
+
+    public function invoices(): HasMany
+    {
+        return $this->hasMany(Invoice::class);
+    }
+
+    public function statements(): MorphMany
+    {
+        return $this->morphMany(AccountStatement::class, 'accountable');
+    }
+
+    public function payments(): MorphMany
+    {
+        return $this->morphMany(Payment::class, 'accountable');
+    }
+}
---
+++ app/Policies/TenantPolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Tenant;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class TenantPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return $user->can('list tenants');
+    }
+
+    public function view(User $user, Tenant $tenant): bool
+    {
+        return $user->can('view tenant');
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->can('create tenant');
+    }
+
+    public function update(User $user, Tenant $tenant): bool
+    {
+        return $user->can('edit tenant');
+    }
+
+    public function delete(User $user, Tenant $tenant): bool
+    {
+        return $user->can('delete tenant');
+    }
+
+}
