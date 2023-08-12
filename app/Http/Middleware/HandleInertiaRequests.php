97298e6ba2697e3b6b94985ea475056ddd61b12a
waitaamon <waitaamon@gmail.com>
2023-08-12 10:11:54 +0300

    added properties to global props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Property;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -40,6 +41,7 @@ class HandleInertiaRequests extends Middleware
             'flash' => [
                 'toast' => $request->session()->get('toast'),
             ],
+            'properties' => Property::select('id', 'name')->get()
         ]);
     }
 }
